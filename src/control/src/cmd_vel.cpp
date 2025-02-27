5b0704584bdc2a19179e6a9f3d1a0aa14456c6ca
johntime2005 <johntimeson@outlook.com>
2025-02-27 18:41:08 +0800

    修复移动函数相关问题

--- src/control/src/cmd_vel.cpp
+++ src/control/src/cmd_vel.cpp
@@ -4,8 +4,8 @@
 #include "std_msgs/msg/float32.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include "control/canbus.h" // 假设你的 CAN 库头文件是 control/canbus.h
-#include <string.h>         // 引入 memset
+#include "control/canbus.h"
+#include <string.h>
 #include <stdio.h>
 
 int dev = 0;
@@ -88,20 +88,20 @@ public:
     }
 
 private:
+    rclcpp::TimerBase::SharedPtr timer_;
     void calculate_speed(int speed, int rad, unsigned char *data)
     {
-        // 目标档位 (运动学控制模式)
-        data[0] = 0x03;
-
         // 目标速度 (单位: 0.001 m/s)
-        short speed_short = (short)(speed);   // 转换为short int类型
-        data[0] |= (speed_short & 0x0F) << 4; // 低4位,并保留目标档位
-        data[1] = (speed_short >> 4) & 0xFF;  // 高8位
+        short speed_short = (short)(speed);    // 转换为short int类型
+        data[0] |= (speed_short & 0x0F) << 4;  // 低4位,并保留目标档位
+        data[1] |= (speed_short >> 4) & 0xFF;  // 高8位
+        data[2] |= (speed_short >> 12) & 0x0F; // 最高4位
 
         // 目标角速度 (单位: 0.01 °/s)
-        short rad_short = (short)(rad);    // 转换为short int类型
-        data[2] = (rad_short & 0x0F) << 4; // 低4位
-        data[3] = (rad_short >> 4) & 0xFF; // 高8位
+        short rad_short = (short)(rad);      // 转换为short int类型
+        data[2] |= (rad_short & 0x0F) << 4;  // 低4位
+        data[3] |= (rad_short >> 4) & 0xFF;  // 高8位
+        data[4] |= (rad_short >> 12) & 0x0F; // 最高4位
     }
 
     void move(int speed, int rad)
@@ -109,11 +109,12 @@ private:
         // 清零信号结构体
         memset(&txmsg1[0], 0, sizeof(txmsg1[0]));
         txmsg1[0].ID = 0x18C4D1D0;
+        // 目标档位 (运动学控制模式)
+        txmsg1[0].Data[0] = 0x03;
 
         // 填充速度、角速度信号
         calculate_speed(speed, rad, txmsg1[0].Data);
 
-        // Byte 5:  Byte5虽然手册没提，但是安全起见，我们还是设为0
         txmsg1[0].Data[5] = 0;
 
         // Byte 6: Alive Rolling Counter，每发送一帧递增, 取低4位
@@ -136,13 +137,11 @@ private:
         // 发送CAN帧，超时时间设为100ms（具体值可调整）
         CAN_Transmit(dev, cpot0, &txmsg1[0], 1, 100);
 
-        // 调用打印函数输出发送帧内容
         print_frame(&txmsg1[0]);
     }
 
     void timer_callback()
     {
-        // 在这里设置你想要的速度和角速度
         int speed = 0;
         int rad = 30;
         speed = speed * 1000; // 转换为 0.001 m/s
@@ -151,8 +150,6 @@ private:
         // 调用 move 函数发送控制指令
         move(speed, rad);
     }
-
-    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char **argv)
